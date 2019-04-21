335ac998228c87fbc1af17255508d1ac8034d454
zoranm5 <zoran.musulin@gmail.com>
2019-04-21 21:51:29 +0200

    Refractoring and adding numTo String

--- tstut.js
+++ tstut.js
@@ -5,3 +5,12 @@ var anything = "dog";
 anything = 2;
 document.getElementById("tsStuff")
     .innerHTML = "My Name is " + myName;
+document.getElementById("tsStuff")
+    .innerHTML = "My Name is " + myName;
+document.write("canVote is a " + typeof (canVote) + "<br />");
+document.write("myname is a " + typeof (myName) + "<br />");
+document.write("anything is a " + typeof (anything) + "<br />");
+var strToNum = parseInt("5");
+var numToStr = 5;
+document.write("numToStr is a" + typeof (numToStr.toString()) + "<br />");
+var PI = 3.14159;
--- tstut.ts
+++ tstut.ts
@@ -6,3 +6,15 @@ anything = 2;
 
 document.getElementById("tsStuff")
 .innerHTML = "My Name is " + myName;
+document.getElementById("tsStuff")
+    .innerHTML = "My Name is " + myName;
+document.write("canVote is a " + typeof(canVote) + "<br />");
+document.write("myname is a " + typeof(myName) + "<br />");
+document.write("anything is a " + typeof(anything) + "<br />");
+
+var strToNum: number = parseInt("5");
+
+var numToStr: number = 5;
+document.write("numToStr is a" + typeof(numToStr.toString()) + "<br />";
+
+const PI = 3.14159;
