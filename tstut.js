b013798ea2a1e21f734d32ef7f7dc3ee7eb857e5
Zoran Musulin <zoran.musulin@gmail.com>
2019-04-21 20:50:38 +0200

    TypeScript tutorial

---
+++ tstut.js
@@ -0,0 +1,5 @@
+var myName = "Derek";
+var myAge = 41;
+var canVote = true;
+var anything = "dog";
+anything = 2;
---
+++ tstut.ts
@@ -0,0 +1,5 @@
+var myName: string = "Derek";
+var myAge: number = 41;
+var canVote: boolean = true;
+var anything: any = "dog";
+anything = 2;
